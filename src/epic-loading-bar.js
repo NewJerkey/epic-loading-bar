0a424aaddb7dc259ce0b5ad942020a5a5c0b7e1d
NewJerkey <bryschneider1@gmail.com>
2023-04-13 20:57:17 -0400

    css styling and framework for animation added

--- src/epic-loading-bar.js
+++ src/epic-loading-bar.js
@@ -2,7 +2,7 @@ import { LitElement, html, css } from 'lit';
 
 class EpicLoadingBar extends LitElement {
   static properties = {
-    header: { type: String },
+    title: { type: String },
   }
 
   static styles = css`
@@ -12,36 +12,51 @@ class EpicLoadingBar extends LitElement {
       text-align: center;
     }
 
+    .bar-wrapper {
+      border: 1px solid #8e8e8e;
+      border-radius: 15px;
+      justify-content: space-between;
+      display: flex;
+      width: 50%;
+      display: inline-block;
+      vertical-align: middle;
+    }
+
+    .bar-wrapper:hover .loading-bar {
+      width: 99.25%;
+    }
+
     .bar-title {
         display: inline-block;
         font-family: "Inter var experimental", "Inter var", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-        font-color: #8e8e8e;
+        color: #8e8e8e;
         margin-right: 50px;
+        min-width: 180px;
     }
 
     .loading-bar {
-      background: blue;
-      width: 50%;
+      width: 0%;
       height: 50px;
-      border-radius: 25px;
-      display: inline-block;
-      vertical-align: middle;
+      border-radius: 15px;
+      margin: 3px;
+      background: linear-gradient(to right,#d3c357,#f1a76a,#cc6d2e);
+      transition: width 2s;
     }
   `;
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.title = 'Rspack';
   }
 
   render() {
     return html`
       <div class="wrapper">
         <div class="bar-title">
-          <p>Rspack</p>
+          <p>${this.title}</p>
         </div>
-        <div class="loading-bar">
-
+        <div class="bar-wrapper">
+          <div class="loading-bar"></div>
         </div>
       </div>
     `;
---
+++ src/loading-bar-app.js
@@ -0,0 +1,48 @@
+import { LitElement, html, css } from "lit";
+import "./epic-loading-bar.js";
+
+export class BarApp extends LitElement {
+  static get tag() {
+    return "bar-list";
+  }
+
+  static get properties() {
+    return {
+      list: { type: Array },
+    };
+  }
+
+  constructor() {
+    super();
+    this.list = [];
+    this.updateBars();
+  }
+
+  updateBars() {
+    // const address = '../api/badges';
+    const address = "../assets/bar-map.json";
+    fetch(address)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return [];
+      })
+      .then((data) => {
+        this.list = data;
+      });
+  }
+
+  static get styles() {
+    return css`
+
+    `;
+  }
+
+  render() {
+    return html`
+ 
+      `;
+  }
+}
+customElements.define(BarApp.tag, BarApp);
